bfc3245a8f83fa6294c5effcba935a34110b3085
Dan Tong <dan@dtong.nz>
2024-03-18 20:44:02 +1300

    add quantisation/params to additional voices

--- algae.lua
+++ algae.lua
@@ -7,19 +7,29 @@ jf = include("lib/jf")
 kria = include("lib/kria")
 -- txi = include("lib/txi")
 
-scale = musicutil.generate_scale(0, "major pentatonic", 4)
+carve = {
+  -- 5 note scales (pentatonic)
+  [5] = { 1, 4, 2, 5, 3 },
+  -- 7 note scales
+  [7] = { 1, 5, 3, 7, 2, 6, 4 },
+}
 
 function init_params()
   params:add_separator("global")
-  params:add_number("global_scale", "scale", 1, 3, 1)
+  -- TODO: options here for scales to select
+  -- see musicutil docs for good examples on params
+  -- and for building scales from parameter actions
+  -- and parameter formatter
+  params:add_number("global_scale_type", "scale", 1, 3, 1)
   params:add_option("global_root", "root note", generate_circle_of_fifths_names(), 1)
 
   params:add_separator("voices")
   for i = 1, 4 do
     params:add_group("voice " .. i, 4)
     params:add_number("voice_root_offset_" .. i, "root offset", -11, 11, 0)
+    params:add_number("voice_note_offset_" .. i, "note offset", -11, 11, 0)
+    params:add_number("voice_octave_offset_" .. i, "octave offset", -3, 3, 0)
     params:add_number("voice_carve_" .. i, "carve", 0, 4, 0)
-    params:add_number("voice_note_offset_" .. i, "note offset", 0, 11, 0)
     params:add_number("voice_chance_" .. i, "chance", 0, 100, 100)
   end
 
@@ -37,32 +47,22 @@ function init()
 
   init_params()
 
-  kria.cv.event_handlers[1] = function(value)
-    -- TODO: don't generate scale so frequently - params bang/action instead
-    root_index = params:get("global_root")
-    root_offset = params:get("voice_root_offset_1")
-    root = circle_of_fifths_at(root_index + root_offset)
-    scale = musicutil.generate_scale(root, "major pentatonic", 6)
-    print("root note is " .. musicutil.note_num_to_name(root))
-
-    note_offset = params:get("voice_note_offset_1")
-    volts = quantise(value, note_offset, scale)
+  kria.cv.event_handlers[1] = function(cv_value)
+    volts = quantise_note_for_voice(cv_value, 1)
 
     if kria.mute.values[1] == 0 then
       jf.play_note(volts, 4)
     end
   end
 
-  kria.cv.event_handlers[2] = function(value)
+  kria.cv.event_handlers[2] = function(cv_value)
     if kria.mute.values[2] == 0 then
-      -- voice 2
-      offset = params:get("voice_note_offset_2")
-      volts = quantise(value, offset)
+      volts = quantise_note_for_voice(cv_value, 2)
       crow.output[1].volts = volts
+    end
 
-      -- voice 3
-      offset = params:get("voice_note_offset_3")
-      volts = quantise(value, offset)
+    if kria.mute.values[3] == 0 then
+      volts = quantise_note_for_voice(cv_value, voice)
       crow.output[2].volts = volts
     end
   end
@@ -128,10 +128,14 @@ function note_to_volts(note)
   return note * (1 / 12)
 end
 
-function quantise(cv_value, note_offset, scale)
-  if note_offset == nil then
-    note_offset = 0
-  end
+function quantise_note_for_voice(cv_value, voice)
+  root_index = params:get("global_root")
+  scale_type = params:get("global_scale_type")
+  root_offset = params:get("voice_root_offset_" .. voice)
+  note_offset = params:get("voice_note_offset_" .. voice)
+
+  root = circle_of_fifths_at(root_index + root_offset)
+  scale = musicutil.generate_scale(root, scale_type, 6)
 
   note = volts_to_note(cv_value)
   octave = note / 12
@@ -139,7 +143,7 @@ function quantise(cv_value, note_offset, scale)
   note_index = clamped_note + 1 + note_offset
   quantised_note = scale[note_index]
 
-  volts = note_to_volts(quantised_note + octave) -- + octave_volts + util.round(note / #scale)
+  volts = note_to_volts(quantised_note + octave)
   return volts
 end
 
