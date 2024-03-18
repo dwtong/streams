4926f918347237bfc85ea924f2663c33944f05a8
Dan Tong <dan@dtong.nz>
2024-03-18 17:52:52 +1300

    add scale root note and root offset params

--- algae.lua
+++ algae.lua
@@ -12,13 +12,14 @@ scale = musicutil.generate_scale(0, "major pentatonic", 4)
 function init_params()
   params:add_separator("global")
   params:add_number("global_scale", "scale", 1, 3, 1)
+  params:add_option("global_root", "root note", generate_circle_of_fifths_names(), 1)
 
   params:add_separator("voices")
   for i = 1, 4 do
     params:add_group("voice " .. i, 4)
-    params:add_number("voice_root_" .. i, "root note", 1, 12, 1)
+    params:add_number("voice_root_offset_" .. i, "root offset", -11, 11, 0)
     params:add_number("voice_carve_" .. i, "carve", 0, 4, 0)
-    params:add_number("voice_offset_" .. i, "offset", 0, 11, 0)
+    params:add_number("voice_note_offset_" .. i, "note offset", 0, 11, 0)
     params:add_number("voice_chance_" .. i, "chance", 0, 100, 100)
   end
 
@@ -37,8 +38,15 @@ function init()
   init_params()
 
   kria.cv.event_handlers[1] = function(value)
-    offset = params:get("voice_offset_1")
-    volts = quantise(value, offset)
+    -- TODO: don't generate scale so frequently - params bang/action instead
+    root_index = params:get("global_root")
+    root_offset = params:get("voice_root_offset_1")
+    root = circle_of_fifths_at(root_index + root_offset)
+    scale = musicutil.generate_scale(root, "major pentatonic", 6)
+    print("root note is " .. musicutil.note_num_to_name(root))
+
+    note_offset = params:get("voice_note_offset_1")
+    volts = quantise(value, note_offset, scale)
 
     if kria.mute.values[1] == 0 then
       jf.play_note(volts, 4)
@@ -48,12 +56,12 @@ function init()
   kria.cv.event_handlers[2] = function(value)
     if kria.mute.values[2] == 0 then
       -- voice 2
-      offset = params:get("voice_offset_2")
+      offset = params:get("voice_note_offset_2")
       volts = quantise(value, offset)
       crow.output[1].volts = volts
 
       -- voice 3
-      offset = params:get("voice_offset_3")
+      offset = params:get("voice_note_offset_3")
       volts = quantise(value, offset)
       crow.output[2].volts = volts
     end
@@ -73,11 +81,11 @@ end
 
 function key(n, z)
   if n == 3 and z == 1 then
-    reload()
+    r()
   end
 end
 
-function reload()
+function r()
   norns.script.load("code/algae/algae.lua")
 end
 
@@ -120,7 +128,7 @@ function note_to_volts(note)
   return note * (1 / 12)
 end
 
-function quantise(cv_value, note_offset)
+function quantise(cv_value, note_offset, scale)
   if note_offset == nil then
     note_offset = 0
   end
@@ -133,4 +141,22 @@ function quantise(cv_value, note_offset)
 
   volts = note_to_volts(quantised_note + octave) -- + octave_volts + util.round(note / #scale)
   return volts
+end
+
+function circle_of_fifths_at(index)
+  index = (index - 1) % 12
+  if index % 2 == 0 then
+    return index
+  else
+    return (index + 6) % 12
+  end
+end
+
+function generate_circle_of_fifths_names()
+  notes = {}
+  for i = 1, 12 do
+    note_num = circle_of_fifths_at(i)
+    notes[i] = musicutil.note_num_to_name(note_num)
+  end
+  return notes
 end
