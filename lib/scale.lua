907a21890b1af7b6a6f21c0c0d0554cb25ab6567
Dan Tong <dan@dtong.nz>
2024-03-22 12:22:00 +1300

    create carve_scale function

--- lib/scale.lua
+++ lib/scale.lua
@@ -1,10 +1,11 @@
 local Scale = {}
 
-local carve = {
+local carve_priorities = {
   -- 5 note scales (pentatonic)
   [5] = { 1, 4, 2, 5, 3 },
   -- 7 note scales
   [7] = { 1, 5, 3, 7, 2, 6, 4 },
+  -- TODO: handle other scale sizes
 }
 
 function Scale.scale_names()
@@ -12,6 +13,7 @@ function Scale.scale_names()
   for i, scale in ipairs(musicutil.SCALES) do
     scales[i] = string.lower(scale.name)
   end
+
   return scales
 end
 
@@ -19,9 +21,9 @@ function Scale.circle_of_fifths_at(index)
   index = (index - 1) % 12
   if index % 2 == 0 then
     return index
-  else
-    return (index + 6) % 12
   end
+
+  return (index + 6) % 12
 end
 
 function Scale.circle_of_fifths_names()
@@ -31,6 +33,28 @@ function Scale.circle_of_fifths_names()
     notes[i] = musicutil.note_num_to_name(note_num)
   end
   return notes
+end
+
+function Scale.carve_scale(scale_type, carve_amount)
+  local scale_notes = musicutil.generate_scale(0, scale_type, 1)
+  local carved_scale = {}
+
+  for length, priorities in pairs(carve_priorities) do
+    if #scale_notes - 1 == length then
+      for i = 1, length - carve_amount do
+        note_number = priorities[i]
+        carved_scale[i] = scale_notes[note_number]
+      end
+    end
+  end
+
+  if #carved_scale == 0 then
+    print("scale not supported by carve.")
+    return scale_notes
+  end
+
+  table.sort(carved_scale)
+  return carved_scale
 end
 
 return Scale
